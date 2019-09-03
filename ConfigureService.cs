e895591447edd41581f6afba46cf915512404c19
danielramos <daniel.ap@repom.com.br>
2019-09-03 11:47:24 -0300

    Criação da branch

---
+++ ConfigureService.cs
@@ -0,0 +1,27 @@
+﻿using Topshelf;
+
+namespace WindowsServiceWithTopshelf
+{
+    internal static class ConfigureService
+    {
+        internal static void Configure()
+        {
+            HostFactory.Run(
+                configure =>
+                {
+                    configure.Service<MyService>(
+                        service =>
+                        {
+                            service.ConstructUsing(s => new MyService());
+                            service.WhenStarted(s => s.Start());
+                            service.WhenStopped(s => s.Stop());
+                        });
+                    configure.RunAsLocalSystem();
+                    configure.SetServiceName("MyWindowServiceWithTopshelf");
+                    configure.SetDisplayName("MyWindowServiceWithTopshelf");
+                    configure.SetDescription("My .Net windows service with Topshelf");
+                }
+                );
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WindowsServiceWithTopshelf
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            ConfigureService.Configure();
+        }
+    }
+}
